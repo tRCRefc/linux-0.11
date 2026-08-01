6f1c79781fd816a3486ef0d91aae68582b5669ef
tarvis <ryanchen42@proton.me>
2026-08-01 19:47:44 +0800

    x86_64: install early descriptor tables

--- arch/x86_64/kernel/main.c
+++ arch/x86_64/kernel/main.c
@@ -6,6 +6,7 @@ x86_64_kernel_main(const struct boot_info *boot_info)
 {
     serial_write("x86-64 kernel stack installed\r\n");
     serial_write("x86-64 kernel main reached\r\n");
+    serial_write("x86-64 descriptor tables installed\r\n");
 
     if (boot_info == 0 || boot_info->memory_map == 0 ||
         boot_info->memory_descriptor_size == 0) {
