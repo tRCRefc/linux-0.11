bba83d47dd9be47d57b21e9c72dcbf40b020765b
tarvis <ryanchen42@proton.me>
2026-08-02 15:03:11 +0800

    mm: add early memory map

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3,6 +3,8 @@
 
 #define PAGE_SIZE 4096
 
+extern void mem_init(unsigned long start_mem, unsigned long end_mem);
+extern unsigned long nr_free_pages(void);
 extern unsigned long get_free_page(void);
 extern unsigned long put_page(unsigned long page,unsigned long address);
 extern void free_page(unsigned long addr);
